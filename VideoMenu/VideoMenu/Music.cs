ebed6e9ddff39f3478ae209b5824a6e8c61ba846
kopicon <boldi123@gmail.com>
2017-08-24 15:18:03 +0200

    First completed project
    Able to:
    -Show all the musics
    -Add music:
    -Delet music
    -Modify music

---
+++ VideoMenu/VideoMenu/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoMenu
+{
+    class Music
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Style { get; set; }
+    }
+}
